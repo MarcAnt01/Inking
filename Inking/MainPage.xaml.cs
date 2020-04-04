1736eacd1c704ba3e8326d7f0feda7928f7ae2df
Filippo <fedeli.filippo@gmail.com>
2020-04-04 10:53:25 +0200

    Save and load buttons/functionality

--- Inking/MainPage.xaml.cs
+++ Inking/MainPage.xaml.cs
@@ -30,5 +30,94 @@ namespace Inking
         {
             this.InitializeComponent();
         }
+
+        // Begin "Step 7: Save and load ink"
+        private async void buttonSave_ClickAsync(object sender, RoutedEventArgs e)
+        {
+        //    // Get all strokes on the InkCanvas.
+            IReadOnlyList<InkStroke> currentStrokes = inkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+
+            if (currentStrokes.Count > 0)
+            {
+                // Use a file picker to identify ink file.
+                Windows.Storage.Pickers.FileSavePicker savePicker =
+                    new Windows.Storage.Pickers.FileSavePicker();
+                savePicker.SuggestedStartLocation =
+                    Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+                savePicker.FileTypeChoices.Add(
+                    "GIF with embedded ISF",
+                    new List<string>() { ".gif" });
+                savePicker.DefaultFileExtension = ".gif";
+                savePicker.SuggestedFileName = "InkSample";
+
+                // Show the file picker.
+                Windows.Storage.StorageFile file =
+                    await savePicker.PickSaveFileAsync();
+                // When selected, picker returns a reference to the file.
+                if (file != null)
+                {
+                    // Prevent updates to the file until updates are 
+                    // finalized with call to CompleteUpdatesAsync.
+                    Windows.Storage.CachedFileManager.DeferUpdates(file);
+                    // Open a file stream for writing.
+                    IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+                    // Write the ink strokes to the output stream.
+                    using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
+                    {
+                        await inkCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                        await outputStream.FlushAsync();
+                    }
+                    stream.Dispose();
+
+                    // Finalize write so other apps can update file.
+                    Windows.Storage.Provider.FileUpdateStatus status =
+                        await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+
+                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        // File saved.
+                    }
+                    else
+                    {
+                        // File couldn't be saved.
+                    }
+                }
+                // User selects Cancel and picker returns null.
+                else
+                {
+                    // Operation cancelled.
+                }
+            }
+        }
+
+        private async void buttonLoad_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            // Use a file picker to identify ink file.
+            Windows.Storage.Pickers.FileOpenPicker openPicker =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            openPicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".gif");
+            // Show the file picker.
+            Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+            // When selected, picker returns a reference to the file.
+            if (file != null)
+            {
+                // Open a file stream for reading.
+                IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                // Read from file.
+                using (var inputStream = stream.GetInputStreamAt(0))
+                {
+                    await inkCanvas.InkPresenter.StrokeContainer.LoadAsync(inputStream);
+                }
+                stream.Dispose();
+            }
+            // User selects Cancel and picker returns null.
+            else
+            {
+                // Operation cancelled.
+            }
+        }
+        // End "Step 7: Save and load ink"
     }
 }
